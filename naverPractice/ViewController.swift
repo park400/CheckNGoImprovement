f5e5614e779be9655d06a9d672826ec88f303c81
Minwoo Park <park400@indiana.edu>
2017-06-18 14:37:03 +0900

    Initial Commit

---
+++ naverPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  naverPractice
+//
+//  Created by Minwoo Park on 6/18/17.
+//  Copyright © 2017 MWdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
